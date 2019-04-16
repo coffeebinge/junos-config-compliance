1a655bab0c89a6e52e86b35482c8835f6d3a374a
Bob Hayes <rwhayes@jpl.nasa.gov>
2019-04-16 14:57:18 -0700

    adding sorting for input files and rendered output

--- junos-diff.py
+++ junos-diff.py
@@ -8,11 +8,11 @@ device_config = os.sys.argv[2]
 
 
 with open('{}'.format(gold_config)) as gold:
-    lines1 = gold.readlines()
+    lines1 = sorted(gold.readlines())
 with open('{}'.format(device_config)) as device:
-    lines2 = device.readlines()
+    lines2 = sorted(device.readlines())
 
-for line in difflib.unified_diff(lines1, lines2, fromfile='file1', tofile='file2', lineterm='', n=0):
+for line in sorted(difflib.unified_diff(lines1, lines2, fromfile='file1', tofile='file2', lineterm='', n=0)):
     for prefix in ('---', '+++', '@@', '#'):
         if line.startswith(prefix):
             break
@@ -20,24 +20,28 @@ for line in difflib.unified_diff(lines1, lines2, fromfile='file1', tofile='file2
             break
         elif line.strip().startswith('+set snmp v3 usm'):
             break
-        elif line.strip().startswith('-set'):
-            line = line.replace('-set','set')
         elif line.strip().startswith('+set'):
-            line = line.replace('+set','delete')
-            
+            line = re.sub('^\+set', 'delete', line, flags=re.M)
+        elif line.strip().startswith('-set'):
+            line = re.sub('^\-set', 'set', line, flags=re.M)
     else:
         print line,
-#    else:
-#        print line,
+
 
 
 #####
 ## need to ignore "set version x.x.x" in configs, use another method to verify correct junos version
 ## need to handle deactivated sections of code... if deactivated, throw warning to intervene
-## need to present a message saying  "device in compliance statement" and have follow up 
+## need to present a message saying  "device in compliance statement" and have follow up
 ##   ansible playbook play take that message and stop moving forward
 ## later, add in collection of device config so that if we ever use apply-groups, we include
 ##   the "display inheritence" extra bits so items are not lost. (if needed??-- jinja will likely present the same data)
 ## will need to deal with strings being quoted in jinja output vs not quoted in junos config output
 ## ***WILL also need to deal with delete then set order
 ## *** also, need to exclude snmp v3 users since the stored key will always be different
+
+#old
+#        elif line.strip().startswith('+set'):
+#            line = line.replace('+set','delete')
+#        elif line.strip().startswith('-set'):
+#            line = line.replace('-set','set')
